36895c34ae487a136eaecf9c5c7e232dd5cf5c11
granitrocky <J.Kenyon@ordinarygizmos.com>
2026-06-21 15:39:21 -0500

    Revert "Update publish to build"
    This reverts commit c8c70ed5dc2f60fc9a405a78a1feb4b64a4240af.

--- native/cmake/zig-aarch64-android.cmake
+++ native/cmake/zig-aarch64-android.cmake
@@ -1,6 +1,5 @@
-set(TARGET_TRIPLE "aarch64-linux-musl")
+set(TARGET_TRIPLE "aarch64-linux-android21")
 set(CMAKE_SYSTEM_NAME "Android")
-set(CMAKE_SYSTEM_VERSION "30")
 set(CMAKE_SYSTEM_PROCESSOR "aarch64")
 
 include("${CMAKE_CURRENT_LIST_DIR}/zig-base.cmake")
--- native/cmake/zig-x86_64-android.cmake
+++ native/cmake/zig-x86_64-android.cmake
@@ -1,6 +1,5 @@
-set(TARGET_TRIPLE "x86_64-linux-musl")
+set(TARGET_TRIPLE "x86_64-linux-android21")
 set(CMAKE_SYSTEM_NAME "Android")
-set(CMAKE_SYSTEM_VERSION "30")
 set(CMAKE_SYSTEM_PROCESSOR "x86_64")
 
 include("${CMAKE_CURRENT_LIST_DIR}/zig-base.cmake")
